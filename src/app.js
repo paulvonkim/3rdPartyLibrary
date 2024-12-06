2cee172424480b8abb77cf3200e43c6993d598f7
Paul Kim <ppaauullkkiimm@gmail.com>
2024-12-06 10:07:18 +0100

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-import * as Leaflet from "leaflet";
-
-console.log(Leaflet);
+// import * as Leaflet from 'leaflet'; //duplicate
+// console.log(Leaflet);
 
 import Leaflet from "leaflet"; // import everything from leaflet
 import "leaflet/dist/leaflet.css"; // import leaflet css
@@ -18,3 +17,39 @@ Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map); // add a tile layer to the map, the tiles are those images
+//my locations
+const myLocations = [
+  {
+    name: "WBS CODING SCHOOL",
+    location: [52.457131, 13.54007],
+    description: "The best coding school in the world",
+  },
+  {
+    name: "Alexanderplatz",
+    location: [52.521918, 13.413215],
+    description: "The most famous square in Berlin",
+  },
+  {
+    name: "Brandenburg Gate",
+    location: [52.516275, 13.377704],
+    description: "The most famous gate in Berlin",
+  },
+  {
+    name: "Berlin Wall",
+    location: [52.507541, 13.39032],
+    description: "The most famous wall in Berlin",
+  },
+];
+
+// Add markers to the map with a popup
+myLocations.forEach((location) => {
+  Leaflet.marker(location.location, { icon: markerIcon })
+    .bindPopup(location.description)
+    .addTo(map);
+});
+
+// Set the view to the bounds of all markers
+const bounds = Leaflet.latLngBounds(
+  myLocations.map((location) => location.location)
+);
+map.fitBounds(bounds);
